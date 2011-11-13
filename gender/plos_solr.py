eb1dd3c9786b764bf2e28e755cbd4438e8fad252
Alex Kudlick <adkudlick@gmail.com>
2011-11-12 16:32:32 -0800

    added plos solr module

---
+++ gender/plos_solr.py
@@ -0,0 +1,64 @@
+import json
+from urllib2 import urlopen, quote
+
+searchUrl = 'http://api.plos.org/search?'
+api_key = open('plos_api_key').read()
+
+def search(query='*:*'):
+	'''
+		Basic Solr search functionality.
+
+		This takes in a string or dictionary.  If a string is passed, it is assumed to be basic search terms; 
+		and if a dictionary is passed, the arguments are passed to solr.
+
+		Returns a list containing dictionary objects for each article found. 
+	'''
+
+	if isinstance(query,str): 
+		query = { 'q' : quote(query) }	
+	else:
+		if not query.has_key('q'): query['q'] = '*:*' #make sure we include a 'q' parameter
+	query['wt'] = 'json' #make sure the return type is json
+	query['fq'] = quote('doc_type:full AND !article_type_facet:"Issue Image"') #search only for articles
+	query['api_key'] = api_key
+	
+	url = searchUrl;
+
+	for part in query:
+		url += '%s%s=%s' % ('&' if url is not searchUrl else '',part,query[part])
+	print 'Making request to',url #TEST
+	print
+	return json.load(urlopen(url),encoding='UTF-8')['response']['docs']
+
+def singlefield(field,numrows=10):
+    '''
+    Query for a single field. Can optionally specify the number of results to return
+    '''
+    query = {
+        'fl' : field,
+        'rows' : numrows
+    }
+    return [f[field] for f in search(query) if f.has_key(field)]
+
+def byIds(dois,fields='title,abstract'):
+    '''
+    Get the specified fields for the specified dois
+    '''
+    if(isinstance(dois,str)): dois = dois.split(',')
+    if isinstance(fields,list): fields = ','.join(fields)
+    if 'id' not in fields: fields = 'id,' + fields
+
+    query = quote(' OR ').join([ 'id:%s' % (f) for f in dois])
+    return search({'q' : query, 'fl': fields})
+
+def ids(limit=10,offset=0):
+    '''
+    Get a list of dois from solr
+    '''
+    results = search({
+        'fl' : 'id',
+        'start' : offset,
+        'rows' : limit,
+        'sort' : quote('publication_date desc')
+        })
+    return [r['id'] for r in results]
