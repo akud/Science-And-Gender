209f83e57c4621fb09bb36813c87afe8ec4bd6ce
Alex Kudlick <adkudlick@gmail.com>
2011-11-13 11:25:24 -0800

    fixed issues with querying the wikipedia api

--- gender/sources/wikipedia_api.py
+++ gender/sources/wikipedia_api.py
@@ -1,6 +1,5 @@
-# library for handling http request for Ushahidi API JSON data
-import httplib
-import csv
+import urllib2 as url
+import csv, json
 
 # get list of categories, store as variables
 _catsF = [f for f in csv.reader(open('data/categories/wikipedia_fname_categories.csv'))]
@@ -16,53 +15,19 @@ _namesFdump= []
 
 # pass values for API request to get XML response
 # But need to loop through all the categories
-for row in _catsF:
+for row in _catsF[0:2]:
 	# store a connection
-	conn = httplib.HTTPConnection("en.wikipedia.org")
-	# write the correct url
-	caturl = "/w/api.php?action=query&list=categorymembers&cmnamespace=0&cmlimit=100&cmtitle=Category:" + str(row[0])
-#	print caturl # Debugging
-	# form the request
-	conn.request("GET", caturl)
-	# store the response
-	r1 = conn.getresponse()
-	# add this to our running tally of dump
-#	_namesFdump = _namesFdump + [r1.read()]
-	print r1.read()
+	caturl = "http://en.wikipedia.org/w/api.php?format=json&action=query&list=categorymembers"+\
+		"&cmnamespace=0&cmlimit=100&cmtitle=Category:" + url.quote(row[0])
+	postsParsed = json.load(url.urlopen(caturl))
+	print json.dumps(postsParsed,indent=2)
 
-# print _namesFdump
 
-# actually get info, store in a variable
-# r1 = conn.getresponse()
-''' print r1.status, r1.reason # Debugging'''
-# use read() to get data, store in variable
-# postsJSON = r1.read()
-# print postsJSON # Debugging
-
-# try to import json library, if can't, import alternative simplejson
-try:
-    import json
-except ImportError:
-    import simplejson as json
-
-# use json.loads() to parse the JSON data into something python-readable
-postsParsed = json.loads(postsJSON)
-# print postsParsed # Debugging
-
-# sampleCaption = postsParsed[u'response'][u'posts'][1][u'caption'] # Debugging
-# print sampleCaption # Debugging
-
-# function to return "captions" from python-readable data.
 def captions(posts):
-    allCaptions = '' # Var to store the full list
-    justPosts = postsParsed[u'response'][u'posts'] # Var to grab just the post objects
-    for r in justPosts:
-        allCaptions = allCaptions + r[u'caption']
-    return allCaptions
-
-# print returned captions
-# print(captions(postsParsed))
-
-# newfile = open('text.txt', mode="w")
-# newfile.write(captions(postsParsed))
-
+	'''Return "captions" from python-readable data.
+	'''
+	allCaptions = '' # Var to store the full list
+	justPosts = postsParsed[u'response'][u'posts'] # Var to grab just the post objects
+	for r in justPosts:
+		allCaptions = allCaptions + r[u'caption']
+	return allCaptions
---
+++ gender/test/test_wikipedia_api.py
@@ -0,0 +1,9 @@
+import unittest
+import gender.sources.wikipedia_api as wikipedia_api
+
+class TestWikipediaApi(unittest.TestCase):
+	def runTest(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
